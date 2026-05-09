6cf3fad7922ddf3fef709166415721b25e37acdc
Sandor Teleki <sandorteleki1@gmail.com>
2026-05-09 20:08:29 +0200

    feat: shots destroy asteroids

--- main.py
+++ main.py
@@ -34,6 +34,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
